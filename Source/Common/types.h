9e2d94d39ee1a1ffd57063ce8cda47a5b6dc79f9
Ilya Kompaniets <jenkslex@gmial.com>
2017-11-03 17:27:22 +0300

    Added simple memory manager.

--- Source/Common/types.h
+++ Source/Common/types.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <cstdint>
 
-typedef uint32_t uint32;
-typedef uint16_t uint16;
-typedef uint8_t uint8;
+typedef uint32_t dword;
+typedef uint16_t word;
+typedef uint8_t byte;
 
 namespace PDP11Emulator::Common
 {
